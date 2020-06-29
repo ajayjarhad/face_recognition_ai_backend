a1736fa7c8a14b162aa1e7f137a0a00876b3c989
Ajay Jarhad <ajayjarhad1@gmail.com>
2020-06-30 05:29:45 +0530

    Modified database linking

--- server.js
+++ server.js
@@ -18,10 +18,8 @@ app.use(cors())
 
 const db = knex({client: 'pg',
 connection: {
-  host : '127.0.0.1',
-  user : 'postgres',
-  password : 'pass',
-  database : 'face_recognition'
+  connectionString:process.env.DATABASE_URL,
+  ssl= true
 }})
 
 app.get('/',(req,res)=>{res.send('Everything is up')})
